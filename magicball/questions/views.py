415592f4a90a16b9bb7ea8e2748a6439dbb788e2
Oleg <107803647+SadFox16@users.noreply.github.com>
2022-07-05 14:39:40 +0300

    Add files via upload

--- magicball/questions/views.py
+++ magicball/questions/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .models import *
+from .services import *
 
 from questions.serializers import (
     LoginSerializer,
@@ -16,40 +16,6 @@ from questions.serializers import (
     UserSerializer,
     QuestionSerializer
     )
-
-
-def get_question_count(question):
-    question_cnt = History.objects.filter(question=question).count()
-    return question_cnt
-
-
-def get_random_answer():
-   ids = Answers.objects.all().order_by('?').values_list('pk', flat=True)
-   pk = random.randint(0, len(ids)-1)
-   rand = Answers.objects.get(pk=ids[pk])
-   return rand
-
-
-def get_last_answer(question, user, answer_id):
-    answer = History.objects.filter(question=question, user=user).order_by('-created_date').first()
-    if answer and answer.id == answer_id:
-        return get_last_answer(question, user, get_random_answer().id)
-    return answer
-
-
-def get_answer(user, question):
-    answer = get_random_answer()
-    history = History(
-        user=user,
-        answer=get_random_answer(),
-        question=question,
-    )
-    history.save()
-    data = {
-        'answer': history.answer.answer,
-        'question_cnt': get_question_count(history.question)
-    }
-    return data
 
 
 class AnswerView(generics.CreateAPIView):
