4aa8ed9fbbb12291fe928e4fbc37d31d6493a9a6
Oleg <107803647+SadFox16@users.noreply.github.com>
2022-07-05 15:05:35 +0300

    Update views.py

--- magicball/questions/views.py
+++ magicball/questions/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
 
 from questions.services import get_question_count, get_random_answer, get_last_answer, get_answer
 
